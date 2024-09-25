97a97455822807f93119aed7c62f68532b089742
zoeyluau <johnvanty2020e@gmail.com>
2024-09-25 15:35:07 +0700

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -2,7 +2,7 @@
 -- 0866
 -- October 31, 2020
 
-local App = loadstring(game:GetHttpAsync("https://raw.githubusercontent.com/zoeyluau/MidiPlayer/refs/heads/main/src/Components/App.lua"))()
+local App = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/zoeyluau/MidiPlayer/refs/heads/main/src/Components/App.lua"))()
 
 if (not isfolder("midi")) then
     makefolder("midi")
