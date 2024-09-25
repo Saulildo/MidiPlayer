3a09646b75d05a6f0e17e0c144917f622ffa8145
zoeyluau <johnvanty2020e@gmail.com>
2024-09-25 15:51:12 +0700

    update

--- src/Components/Controller.lua
+++ src/Components/Controller.lua
@@ -4,7 +4,8 @@
 
 
 
-local midiPlayer = script:FindFirstAncestor("MidiPlayer")
+--local midiPlayer = script:FindFirstAncestor("MidiPlayer")
+
 local Signal = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/zoeyluau/MidiPlayer/refs/heads/main/src/Util/Signal.lua"))()
 local Date = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/zoeyluau/MidiPlayer/refs/heads/main/src/Util/Date.lua"))()
 local Thread = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/zoeyluau/MidiPlayer/refs/heads/main/src/Util/Thread.lua"))()
@@ -20,7 +21,8 @@ local Controller = {
 local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 
-local main, controller
+_G.main = nil
+_G.controller = nil
 
 
 function Controller:Select(filePath)
@@ -38,34 +40,34 @@ function Controller:Update()
     local song = self.CurrentSong
 
     if (song) then
-        main.Title.Text = song.Name
+        _G.main.Title.Text = song.Name
 
         if (song.TimePosition) then
             local date = Date.new(song.TimePosition)
-            controller.Time.Text = ("%s:%s"):format(
+            _G.controller.Time.Text = ("%s:%s"):format(
                 tostring(date.Minute),
                 ("%02i"):format(tostring(date.Second % 60))
             )
         end
 
-        controller.Scrubber.Progress.Size = UDim2.fromScale(math.min(1, song.TimePosition / song.TimeLength), 1)
-        controller.Scrubber.Fill.Size = UDim2.fromScale(1 - controller.Scrubber.Progress.Size.X.Scale, 1)
-        controller.Resume.Image = (song.IsPlaying and "rbxassetid://5915789609") or "rbxassetid://5915551861"
+        _G.controller.Scrubber.Progress.Size = UDim2.fromScale(math.min(1, song.TimePosition / song.TimeLength), 1)
+        _G.controller.Scrubber.Fill.Size = UDim2.fromScale(1 - _G.controller.Scrubber.Progress.Size.X.Scale, 1)
+        _G.controller.Resume.Image = (song.IsPlaying and "rbxassetid://5915789609") or "rbxassetid://5915551861"
 
     else
-        main.Title.Text = "No song selected"
-        controller.Time.Text = "0:00"
-        controller.Scrubber.Progress.Size = UDim2.fromScale(0, 1)
-        controller.Scrubber.Fill.Size = UDim2.fromScale(1, 1)
-        controller.Resume.Image = "rbxassetid://5915551861"
+        _G.main.Title.Text = "No song selected"
+        _G.controller.Time.Text = "0:00"
+        _G.controller.Scrubber.Progress.Size = UDim2.fromScale(0, 1)
+        _G.controller.Scrubber.Fill.Size = UDim2.fromScale(1, 1)
+        _G.controller.Resume.Image = "rbxassetid://5915551861"
     end
 end
 
 
 function Controller:Init(frame)
 
-    main = frame.Main
-    controller = main.Controller
+    _G.main = frame.Main
+    _G.controller = _G.main.Controller
 
     self:_startScrubber()
 
@@ -87,7 +89,7 @@ end
 
 
 function Controller:_startHidePreviewButton()
-    local togglePreview = main.TogglePreview
+    local togglePreview = _G.main.TogglePreview
     togglePreview.MouseButton1Down:Connect(function()
         getgenv()._hideSongPreview = (not getgenv()._hideSongPreview)
         if (getgenv()._hideSongPreview) then
@@ -100,7 +102,7 @@ end
 
 
 function Controller:_startPlaybackButton()
-    local playback = controller.Resume
+    local playback = _G.controller.Resume
     playback.MouseButton1Down:Connect(function()
         if (not self.CurrentSong) then return end
         if (self.CurrentSong.IsPlaying) then
@@ -117,21 +119,21 @@ function Controller:_startScrubber()
 
     -- https://devforum.roblox.com/t/draggable-property-is-hidden-on-gui-objects/107689/5
 
-    local absSize = controller.Scrubber.AbsoluteSize
+    local absSize = _G.controller.Scrubber.AbsoluteSize
 
     local dragging
     local dragInput
 
     local function update(input)
         local song = self.CurrentSong
-        local absPos = controller.Scrubber.AbsolutePosition
+        local absPos = _G.controller.Scrubber.AbsolutePosition
         if (song) then
             song:JumpTo(math.clamp((input.Position.X - absPos.X) / absSize.X, 0, 1) * song.TimeLength)
         end
         self:Update()
     end
 
-    controller.Scrubber.Hitbox.InputBegan:Connect(function(input)
+    _G.controller.Scrubber.Hitbox.InputBegan:Connect(function(input)
         if (input.UserInputType == Enum.UserInputType.MouseButton1) then
             dragging = true
             input.Changed:Connect(function()
@@ -143,7 +145,7 @@ function Controller:_startScrubber()
         end
     end)
 
-    controller.Scrubber.Hitbox.InputChanged:Connect(function(input)
+    _G.controller.Scrubber.Hitbox.InputChanged:Connect(function(input)
         if (input.UserInputType == Enum.UserInputType.MouseMovement) then
             dragInput = input
         end
